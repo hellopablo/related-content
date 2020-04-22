b54df928cb19992947b5955018ff4749cde740e4
Pablo de la Peña <p@shedcollective.org>
2020-04-22 22:49:20 +0100

    Adds index, delete and read methods to engine

--- src/Engine.php
+++ src/Engine.php
@@ -42,4 +42,66 @@ class Engine
     {
         return $this->store;
     }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Indexes an item into the data store
+     *
+     * @param object              $item     The item to index
+     * @param Interfaces\Analyser $analyser The analyser to use
+     *
+     * @return $this
+     */
+    public function index(object $item, Interfaces\Analyser $analyser): self
+    {
+        $relations = $analyser->analyse($item);
+        $id        = $analyser->getId($item);
+
+        $this->store
+            ->delete($analyser, $id)
+            ->write($analyser, $id, $relations);
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Deletes all relationships for an item
+     *
+     * @param object              $item     The item being deleted
+     * @param Interfaces\Analyser $analyser The analyser to use
+     *
+     * @return $this
+     */
+    public function delete(object $item, Interfaces\Analyser $analyser): self
+    {
+        $this->store
+            ->delete(
+                $analyser,
+                $analyser->getId($item)
+            );
+
+        return $this;
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns  all relationships for an item
+     *
+     * @param object              $item     The item being read
+     * @param Interfaces\Analyser $analyser The analyser to use
+     *
+     * @return Interfaces\Relation[]
+     */
+    public function read(object $item, Interfaces\Analyser $analyser): array
+    {
+        return $this->store
+            ->read(
+                $analyser,
+                $analyser->getId($item)
+            );
+    }
 }
