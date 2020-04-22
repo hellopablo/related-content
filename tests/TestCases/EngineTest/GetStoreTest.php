cc0dfd43abd41fd553230a6a36841938d8f9264a
Pablo de la Peña <p@shedcollective.org>
2020-04-22 22:53:18 +0100

    Adds test cases for the Engine::getStore method

---
+++ tests/TestCases/EngineTest/GetStoreTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\TestCases\EngineTest;
+
+use HelloPablo\RelatedContentEngine\Engine;
+use HelloPablo\RelatedContentEngine\Interfaces;
+use PHPUnit\Framework\TestCase;
+use Tests\Mocks;
+
+/**
+ * Class GetStoreTest
+ *
+ * @package Tests\TestCases
+ */
+class GetStoreTest extends TestCase
+{
+    /** @var Interfaces\Store */
+    static $oStore;
+
+    /** @var Engine */
+    static $oEngine;
+
+    // --------------------------------------------------------------------------
+
+    public static function setUpBeforeClass(): void
+    {
+        static::$oStore  = new Mocks\Store([]);
+        static::$oEngine = new Engine(static::$oStore);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \HelloPablo\RelatedContentEngine\Engine::getStore
+     */
+    public function test_returns_instance_of_store()
+    {
+        $this->assertSame(
+            static::$oStore,
+            static::$oEngine->getStore()
+        );
+    }
+}
