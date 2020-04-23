e132dc6dcb11fd26e43118ea6c0fc82685740d25
Pablo de la Peña <p@shedcollective.org>
2020-04-23 12:49:05 +0100

    Ephemeral: Adds disconnect tests

--- tests/TestCases/StoreTest/EphemeralTest/DisconnectTest.php
+++ tests/TestCases/StoreTest/EphemeralTest/DisconnectTest.php
@@ -2,24 +2,35 @@
 
 namespace Tests\TestCases\StoreTest\EphemeralTest;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
-use Tests\TestCases\StoreTest\MySQLTest;
+use HelloPablo\RelatedContentEngine\Store;
 
 /**
  * Class DisconnectTest
  *
- * @package Tests\TestCases\StoreTest\MySQLTest
+ * @package Tests\TestCases\StoreTest\EphemeralTest
  */
 class DisconnectTest extends TestCase
 {
     /**
-     * @covers \HelloPablo\RelatedContentEngine\Store\MySQL::connect
-     * @covers \HelloPablo\RelatedContentEngine\Store\MySQL::disconnect
-     * @covers \HelloPablo\RelatedContentEngine\Store\MySQL::isConnected
-     * @covers \HelloPablo\RelatedContentEngine\Store\MySQL::getConnection
+     * @covers \HelloPablo\RelatedContentEngine\Store\Ephemeral::connect
+     * @covers \HelloPablo\RelatedContentEngine\Store\Ephemeral::disconnect
+     * @covers \HelloPablo\RelatedContentEngine\Store\Ephemeral::isConnected
+     * @covers \HelloPablo\RelatedContentEngine\Store\Ephemeral::getConnection
+     * @throws Exception
      */
-    public function test_can_disconnect_from_mysql()
+    public function test_can_disconnect()
     {
-        $this->markTestIncomplete();
+        $store = new Store\Ephemeral();
+        $store->connect();
+
+        $this->assertTrue($store->isConnected());
+        $this->assertIsArray($store->getConnection());
+
+        $store->disconnect();
+
+        $this->assertFalse($store->isConnected());
+        $this->assertNull($store->getConnection());
     }
 }
