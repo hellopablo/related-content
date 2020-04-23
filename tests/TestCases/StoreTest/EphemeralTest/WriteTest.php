37ce450e5f2f2f813e01d023f2ba17472741daa8
Pablo de la Peña <p@shedcollective.org>
2020-04-23 13:17:01 +0100

    Ephemeral: testing write method returns instance of store interface

--- tests/TestCases/StoreTest/EphemeralTest/WriteTest.php
+++ tests/TestCases/StoreTest/EphemeralTest/WriteTest.php
@@ -3,6 +3,7 @@
 namespace Tests\TestCases\StoreTest\EphemeralTest;
 
 use Exception;
+use HelloPablo\RelatedContentEngine\Interfaces;
 use PHPUnit\Framework\TestCase;
 use Tests\Mocks;
 use Tests\Traits;
@@ -37,5 +38,28 @@ class WriteTest extends TestCase
 
         $data = $store->getConnection();
         $this->assertCount(count($relations), $data);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \HelloPablo\RelatedContentEngine\Store\Ephemeral::write
+     * @throws Exception
+     */
+    public function test_method_returns_instance_of_store()
+    {
+        $store = $this->getStore();
+
+        $analyser  = new Mocks\Analysers\DataTypeOne();
+        $object    = new Mocks\Objects\DataTypeOne1();
+        $id        = $analyser->getId($object);
+        $relations = $analyser->analyse($object);
+
+        $this->assertGreaterThan(0, count($relations));
+
+        $this->assertInstanceOf(
+            Interfaces\Store::class,
+            $store->write($analyser, $id, $relations)
+        );
     }
 }
