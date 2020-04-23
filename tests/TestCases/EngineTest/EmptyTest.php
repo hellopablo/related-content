f0feca17e9b117e0722f9e54ddabdaa2a7eedf3f
Pablo de la Peña <p@shedcollective.org>
2020-04-23 23:42:17 +0100

    Engine: Adds tests for empty method

---
+++ tests/TestCases/EngineTest/EmptyTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\TestCases\EngineTest;
+
+use Exception;
+use HelloPablo\RelatedContentEngine\Engine;
+use HelloPablo\RelatedContentEngine\Interfaces;
+use PHPUnit\Framework\TestCase;
+use Tests\Traits;
+
+/**
+ * Class EmptyTest
+ *
+ * @package Tests\TestCases\EngineTest
+ */
+class EmptyTest extends TestCase
+{
+    use Traits\Stores\Ephemeral;
+
+    // --------------------------------------------------------------------------
+
+    /** @var Interfaces\Store */
+    static $oStore;
+
+    /** @var Engine */
+    static $oEngine;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @throws Exception
+     */
+    public static function setUpBeforeClass(): void
+    {
+        static::$oStore  = static::getStore(['seed' => true]);
+        static::$oEngine = new Engine(static::$oStore);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @covers \HelloPablo\RelatedContentEngine\Engine::empty
+     */
+    public function test_can_empty_store()
+    {
+        $data = static::$oEngine->dump();
+
+        $this->assertNotEmpty($data);
+        $this->assertCount(1, $data);
+
+        static::$oEngine->empty();
+
+        $data = static::$oEngine->dump();
+        $this->assertEmpty($data);
+    }
+}
