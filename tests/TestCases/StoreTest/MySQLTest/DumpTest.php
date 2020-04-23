919f62b0dd4f58963ede8ed233da1959001b6a6f
Pablo de la Peña <p@shedcollective.org>
2020-04-23 23:42:37 +0100

    MySQL: Adds tests for dump method

---
+++ tests/TestCases/StoreTest/MySQLTest/DumpTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\TestCases\StoreTest\MySQLTest;
+
+use Tests\Traits;
+
+/**
+ * Class DumpTest
+ *
+ * @package Tests\TestCases\StoreTest\MySQLTest
+ */
+class DumpTest extends \Tests\TestCases\StoreTest\EphemeralTest\DumpTest
+{
+    use Traits\Stores\MySQL;
+}
