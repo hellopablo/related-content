fe060542e32be12773f2762fd42ba207af466408
Pablo de la Peña <p@shedcollective.org>
2020-04-23 13:01:39 +0100

    Ephemeral: Adds tests for writing data

--- tests/TestCases/StoreTest/EphemeralTest/WriteTest.php
+++ tests/TestCases/StoreTest/EphemeralTest/WriteTest.php
@@ -5,6 +5,7 @@ namespace Tests\TestCases\StoreTest\EphemeralTest;
 use Exception;
 use HelloPablo\RelatedContentEngine\Store;
 use PHPUnit\Framework\TestCase;
+use Tests\Mocks;
 
 /**
  * Class WriteTest
@@ -18,8 +19,19 @@ class WriteTest extends TestCase
      */
     public function test_can_write_data()
     {
-        $this->markTestIncomplete();
         $store = new Store\Ephemeral();
         $store->connect();
+
+        $analyser  = new Mocks\Analysers\DataTypeOne();
+        $object    = new Mocks\Objects\DataTypeOne1();
+        $id        = $analyser->getId($object);
+        $relations = $analyser->analyse($object);
+
+        $this->assertGreaterThan(0, count($relations));
+
+        $store->write($analyser, $id, $relations);
+
+        $data = $store->getConnection();
+        $this->assertCount(count($relations), $data);
     }
 }
